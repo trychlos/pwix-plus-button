a958f2b729349738e4e3ebe48d2c27ce1c988f96
Pierre Wieser <p.wieser@trychlos.org>
2024-06-09 23:57:57 +0200

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:plus-button',
-    version: '1.0.0',
+    version: '1.0.1-rc',
     summary: 'A Blaze component which provides a circled-(+) button',
     git: 'https://github.com/trychlos/pwix-plus-button',
     documentation: 'README.md'
